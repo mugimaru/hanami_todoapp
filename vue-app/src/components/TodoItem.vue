70c4c514e5a15d36a1cd954a7a81cf763169c0e0
mugimaru73 <Mugimaru@live.ru>
2018-04-13 12:22:57 +0400

    add todomvc css/html template

---
+++ vue-app/src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+<li :class="todo.completed ? 'completed' : ''">
+  <div class="view">
+    <input class="toggle" type="checkbox" v-model="todo.completed">
+    <label>{{todo.text}}</label>
+    <button class="destroy"></button>
+  </div>
+  <input class="edit" value="Create a TodoMVC template">
+</li>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoItem',
+  data () {
+    return {}
+  },
+  props: {
+    todo: {}
+  },
+  methods: {
+  }
+}
+</script>
